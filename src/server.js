9c5b9d1501001bdc97cefe24833b7672e8027b54
Marco Novas <marco.nicolas.novas@gmail.com>
2026-07-15 19:35:42 -0300

    categories crud added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import productRouter from "./routes/product.routes.js";
+import categoriesRouter from "./routes/category.routes.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/products", productRouter);
+app.use("/categories", categoriesRouter);
 
 const PORT = process.env.PORT || 3000;
 
